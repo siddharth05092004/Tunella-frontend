46c0d40da60d883ca5f1e46ae0510c4de915985e
Siddharth Amlavad <siddharth05092004@gmail.com>
2025-08-22 23:30:45 +0530

    removed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <>
-    <footer class="footer bg-secondary" >
+    {/* <footer class="footer bg-secondary" >
   <span className="text-gray-400">Made with <span className="text-xl">♥</span> by <a href="https://github.com/siddharth05092004"> <span className="text-gray-200 transition-all duration-25 hover:text-gray-400">Siddharth Amlavad</span> </a></span>
-</footer>
+</footer> */}
     </>
     
   
